dd9f8b0273e8fe4964589b84973f3e3f43e3812e
Max <130847593+fvgdragon16@users.noreply.github.com>
2023-04-16 02:30:55 +0300

    Add files via upload

--- BMI160.ino
+++ BMI160.ino
@@ -1,4 +1,4 @@
-#include <DFRobot_BMI160.h> //подключаем библиотеку для акселерометра + гироскопа BMI160 
+#include <DFRobot_BMI160.h> //подключаем библиотеку для акселерометра + гироскопа BMI160
 
 DFRobot_BMI160 accelerometerGyroscope; //создаем объект accelerometerGyroscope
 
@@ -7,12 +7,12 @@ void setup()
   Serial.begin(9600); //устанавливаем скорость связи 9600
 
   accelerometerGyroscope.softReset(); //инициализируем акселерометр + гироскоп
-  accelerometerGyroscope.I2cInit(0x69); //подключаем датчик акселерометра + гиолскопа к адресу I2C 0x69
+  accelerometerGyroscope.I2cInit(0x69); //подключаем датчик акселерометра + гироскопа к адресу I2C 0x69
 }
 
 void loop()
 {
-  int forAG = 0; //создаем переменую для цикла for(), для калибровки значений акселерометра + гироскопа
+  int forAG = 0; //создаем переменную для цикла for(), для калибровки значений акселерометра + гироскопа
   int valuesAG[6] = {0}; //создаем массив для значений акселерометра + гироскопа; (0 - 2) - значения акселерометра; (3 - 5) - значения гироскопа
 
   accelerometerGyroscope.getAccelGyroData(valuesAG); //записываем данные с акселерометра + гироскопа в массив values
@@ -20,15 +20,15 @@ void loop()
   for(forAG = 0; forAG < 6; forAG++) { //калибруем значения акселерометра + гироскопа
 
     if (forAG < 3) { //калибруем значения гироскопа
-      valuesAG[forAG] = valuesAG[forAG] / 16.4; //переводим згачения в градусы
-      //отладка
+      valuesAG[forAG] = valuesAG[forAG] / 16.4; //переводим значения в градусы
+      //отладка гироскопа
       //Serial.print("A: "); //выводим значения гироскопа
       //Serial.println(valuesAG[forAG]); //вывод данных с осей X, Y и Z  
     }
 
     else { //калибруем значения акселерометра
       valuesAG[forAG] = valuesAG[forAG] / 1670.13; //переводим значения в м/с*с
-      //отладка
+      //отладка акселерометра
       //Serial.print("G: "); //выводим значения акселерометра
       //Serial.println(valuesAG[forAG]); //вывод данных с осей X, Y и Z   
     }
